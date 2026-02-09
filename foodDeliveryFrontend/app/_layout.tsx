ae0406662f9f05129341dbe171cb473fdc1e8247
sahanft <udakarafernando2001@gmail.com>
2026-02-09 18:32:45 +0530

    layout

--- foodDeliveryFrontend/app/_layout.tsx
+++ foodDeliveryFrontend/app/_layout.tsx
@@ -1,24 +1,50 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import "react-native-reanimated";
+import React from "react";
+import { StatusBar } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux"; // Redux Provider
+import { store } from "../store/store"; // Redux store
+import { CartProvider } from "../provider/CartProvider"; // Import CartProvider
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import SplashScreen from "../app/SplashScreen";
+import LoginScreen from "../app/LoginScreen";
+import SignUpScreen from "../app/SignUpScreen";
+import CartScreen from "./CartScreen";
+import FoodDetails from "../app/FoodDetails";
+import AddOrderDetails from "../app/AddOrderDetails";
+import TabNavigator from "./(tabs)/_layout";
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Provider store={store}>
+      <CartProvider>
+        {" "}
+        {/* Wrap the app with CartProvider */}
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {/* Auth Flow */}
+          <Stack.Screen name="Splash" component={SplashScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="AddOrderDetails" component={AddOrderDetails} />
+
+          {/* Main App Flow */}
+          <Stack.Screen name="Main" component={TabNavigator} />
+          <Stack.Screen name="CartScreen" component={CartScreen} />
+          <Stack.Screen name="FoodDetails" component={FoodDetails} />
+        </Stack.Navigator>
+        <StatusBar />
+      </CartProvider>
+    </Provider>
   );
 }
