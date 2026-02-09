66e7cc39f45658c4e59ddba197f5b1434d7517e0
sahanft <udakarafernando2001@gmail.com>
2026-02-09 18:29:38 +0530

    new update

--- foodDeliveryFrontend/app/(tabs)/_layout.tsx
+++ foodDeliveryFrontend/app/(tabs)/_layout.tsx
@@ -1,35 +1,81 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
+import { Platform } from "react-native";
+import HomeScreen from "../(tabs)/index";
+import ExploreScreen from "./ProfileScreen ";
+import OrderScreen from "./OrderScreen";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+const Tab = createBottomTabNavigator();
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function Tabs() {
+  const activeColor = "#009900";
+  const inactiveColor = "#B0B0B0";
 
   return (
-    <Tabs
+    <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
+        tabBarStyle: Platform.select({
+          ios: {
+            position: "absolute",
+            backgroundColor: "white",
+            height: 60,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: -2 },
+            shadowOpacity: 0.1,
+            shadowRadius: 10,
+          },
+          default: {
+            backgroundColor: "white",
+            height: 50,
+            elevation: 5,
+          },
+        }),
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="home"
+              size={28}
+              color={focused ? activeColor : inactiveColor}
+            />
+          ),
         }}
       />
-      <Tabs.Screen
-        name="explore"
+      <Tab.Screen
+        name="OrderScreen"
+        component={OrderScreen}
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "History",
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="history"
+              size={28}
+              color={focused ? activeColor : inactiveColor}
+            />
+          ),
         }}
       />
-    </Tabs>
+      <Tab.Screen
+        name="ProfileScreen"
+        component={ExploreScreen}
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome
+              name="user"
+              size={28}
+              color={focused ? activeColor : inactiveColor}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
--- foodDeliveryFrontend/app/(tabs)/index.tsx
+++ foodDeliveryFrontend/app/(tabs)/index.tsx
@@ -1,98 +1,44 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import "../../global.css";
+import React from "react";
+import OfferCard from "@/components/home/OfferCard";
+import SerchBar from "@/components/home/SerchBar";
+import Icon from "react-native-vector-icons/FontAwesome";
+import FoodCards from "@/components/home/FoodCards";
+import { useNavigation } from "@react-navigation/native";
+import FoodCategories from "@/components/home/FoodCategories";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const navigation = useNavigation();
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  return (
+    <View className="bg-white w-full h-full">
+      <View
+        className="w-full h-96 bg-green-900"
+        style={{ borderBottomRightRadius: 30, borderBottomLeftRadius: 30 }}
+      >
+        <Text className="text-white text-xl font-bold top-20 left-5">
+          Hellow Shanilka!
+        </Text>
+        <Text className="text-white text-3xl font-bold top-20 left-5">
+          Place Your Order
+        </Text>
+        <TouchableOpacity
+          className="absolute w-12 h-12 rounded-full top-20 right-5"
+          onPress={() => navigation.navigate("CartScreen")}
+        >
+          <Icon
+            name="shopping-cart"
+            size={25}
+            color={"#ffffff"}
+            className="top-0 left-0 bottom-0 right-0 m-auto "
+          />
+        </TouchableOpacity>
+        <SerchBar />
+      </View>
+      <OfferCard />
+      <FoodCategories />
+      <FoodCards />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
